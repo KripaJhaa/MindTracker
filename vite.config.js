70b6a101afdeacdfe229b2d8b91453862fb1eac7
kripajha <kripashankarjha9@gmail.com>
2025-09-06 14:30:55 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: process.cwd(),
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    outDir: 'dist'
   }
 })

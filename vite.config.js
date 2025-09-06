1107a84764dbc8af3881b37b6cab5b6e96776239
kripajha <kripashankarjha9@gmail.com>
2025-09-06 13:57:03 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    target: 'es2015',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom']
-        },
-        // Optimize asset naming for caching
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-          if (/\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
-            return `images/[name]-[hash][extname]`;
-          }
-          if (/\.(css)$/i.test(assetInfo.name)) {
-            return `css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js'
-      }
-    },
-    // Enable compression
-    cssCodeSplit: true,
-    reportCompressedSize: false
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
-  preview: {
-    port: 4173,
-    host: true
-  },
-  // SEO and Performance optimizations
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
+    outDir: 'dist'
   }
 })
